0efba678d4fa29273d703bf92954adb2ab2dd37b
owlstechbd <owlstech.net@gmail.com>
2024-03-15 12:50:20 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,291 @@
+
+
+document.querySelector('.menu').addEventListener('click',function(){
+
+    document.querySelector('.xmark').style = "display:flex";
+    document.querySelector('.mm').style = "display:block";
+    document.querySelector('.mm2').style = "display:block";
+    document.querySelector('.mm3').style = "display:block";
+    document.querySelector('.mm4').style = "display:block";
+    document.querySelector('.drop-open5').style = "display:block";
+    document.querySelector('.drop-open6').style = "display:block";
+    document.querySelector('.menu').style = "display:none";
+    document.querySelector('.dot-m').style = "display:none";
+    document.querySelector('.slideshow-container').style = "display:none";
+    document.querySelector('.section-one').style = "display:none";
+    document.querySelector('.section-two').style = "display:none";
+    document.querySelector('.section-three').style = "display:none";
+    document.querySelector('.contact-container').style ="display:none";
+   
+    
+    
+    
+
+})
+
+document.querySelector('.xmark').addEventListener('click',function(){
+
+    document.querySelector('.menu').style = "display:block";
+    document.querySelector('.xmark').style = "display:none";
+    document.querySelector('.mm').style = "display:none";
+    document.querySelector('.mm2').style = "display:none";
+    document.querySelector('.mm3').style = "display:none";
+    document.querySelector('.mm4').style = "display:none";
+    document.querySelector('.drop-open5').style = "display:none";
+    document.querySelector('.drop-open6').style = "display:none";
+    document.querySelector('.dot-m').style = "display:block";
+    document.querySelector('.slideshow-container').style = "display:block";
+    document.querySelector('.section-one').style = "display:block";
+    document.querySelector('.section-two').style = "display:block";
+    document.querySelector('.section-three').style = "display:block";
+    document.querySelector('.contact-container').style ="display:flex";
+    document.querySelector('.foo').style = "display:block";
+   
+
+})
+
+document.querySelector('.r-angle').addEventListener('click',function(){
+
+    document.querySelector('.drop-open').style = "display:block";
+    document.querySelector('.header').style = "display:none";
+    document.querySelector('.mm').style = "display:none";
+    document.querySelector('.mm2').style = "display:none";
+    document.querySelector('.mm3').style = "display:none";
+    document.querySelector('.mm4').style = "display:none";
+    document.querySelector('.drop-open5').style = "display:none";
+    document.querySelector('.drop-open6').style = "display:none";
+
+})
+
+
+document.querySelector('.l-angle').addEventListener('click',function(){
+
+    document.querySelector('.drop-open').style = "display:none";
+    document.querySelector('.header').style = "display:flex";
+    document.querySelector('.mm').style = "display:block";
+    document.querySelector('.mm2').style = "display:block";
+    document.querySelector('.mm3').style = "display:block";
+    document.querySelector('.drop-open5').style = "display:block";
+    document.querySelector('.drop-open6').style = "display:block";
+
+})
+
+document.querySelector('.close').addEventListener('click',function(){
+
+    document.querySelector('.drop-open').style = "display:none";
+    document.querySelector('.header').style = "display:flex";
+    document.querySelector('.mm').style = "display:none";
+    document.querySelector('.mm2').style = "display:none";
+    document.querySelector('.xmark').style = "display:none";
+    document.querySelector('.menu').style = "display:block";
+    document.querySelector('.drop-open5').style = "display:none";
+    document.querySelector('.drop-open6').style = "display:none";
+    document.querySelector('.dot-m').style = "display:block";
+    document.querySelector('.slideshow-container').style = "display:flex";
+    document.querySelector('.section-one').style = "display:block";
+    document.querySelector('.section-two').style = "display:block";
+    document.querySelector('.section-three').style = "display:block";
+    document.querySelector('.contact-container').style ="display:block";
+   
+
+})
+
+            //  drop-dowmn2
+
+
+            document.querySelector('.r-angle2').addEventListener('click',function(){
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:block";
+                document.querySelector('.header').style = "display:none";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.mm3').style = "display:none";
+                document.querySelector('.mm4').style = "display:none";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+            
+
+            document.querySelector('.l-angle2').addEventListener('click',function(){
+
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:block";
+                document.querySelector('.mm2').style = "display:block";
+                document.querySelector('.mm3').style = "display:block";
+                document.querySelector('.mm4').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:block";
+                document.querySelector('.drop-open6').style = "display:block";
+            
+            })
+
+
+            document.querySelector('.close2').addEventListener('click',function(){
+
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.xmark').style = "display:none";
+                document.querySelector('.menu').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+
+
+
+ 
+             
+            //  drop-dowmn3
+
+            document.querySelector('.r-angle3').addEventListener('click',function(){
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:block";
+                document.querySelector('.header').style = "display:none";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.mm3').style = "display:none";
+                document.querySelector('.mm4').style = "display:none";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+            
+
+
+            document.querySelector('.l-angle3').addEventListener('click',function(){
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:block";
+                document.querySelector('.mm2').style = "display:block";
+                document.querySelector('.mm3').style = "display:block";
+                document.querySelector('.mm4').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:block";
+                document.querySelector('.drop-open6').style = "display:block";
+            
+            })
+
+            
+            document.querySelector('.close3').addEventListener('click',function(){
+
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.mm3').style = "display:none";
+                document.querySelector('.xmark').style = "display:none";
+                document.querySelector('.menu').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+
+             
+              //  drop-dowmn4
+
+              document.querySelector('.r-angle4').addEventListener('click',function(){
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:none";
+                document.querySelector('.drop-open4').style = "display:block";
+                document.querySelector('.header').style = "display:none";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.mm3').style = "display:none";
+                document.querySelector('.mm4').style = "display:none";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+
+
+            document.querySelector('.l-angle4').addEventListener('click',function(){
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:none";
+                document.querySelector('.drop-open4').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:block";
+                document.querySelector('.mm2').style = "display:block";
+                document.querySelector('.mm3').style = "display:block";
+                document.querySelector('.mm4').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:block";
+                document.querySelector('.drop-open6').style = "display:block";
+            
+            })
+
+            document.querySelector('.close4').addEventListener('click',function(){
+
+                document.querySelector('.drop-open').style = "display:none";
+                document.querySelector('.drop-open2').style = "display:none";
+                document.querySelector('.drop-open3').style = "display:none";
+                document.querySelector('.drop-open4').style = "display:none";
+                document.querySelector('.header').style = "display:flex";
+                document.querySelector('.mm').style = "display:none";
+                document.querySelector('.mm2').style = "display:none";
+                document.querySelector('.mm3').style = "display:none";
+                document.querySelector('.mm4').style = "display:none";
+                document.querySelector('.xmark').style = "display:none";
+                document.querySelector('.menu').style = "display:block";
+                document.querySelector('.drop-open5').style = "display:none";
+                document.querySelector('.drop-open6').style = "display:none";
+            
+            })
+             
+             
+              
+
+             
+             
+             
+            document.querySelector('.world').addEventListener('click',function(){
+            document.querySelector('.world-mega').style = "display:flex";
+            document.querySelector('.dot-m').style = "display:none";
+
+             
+            
+            })
+             
+
+            document.querySelector('.w-x').addEventListener('click',function(){
+                document.querySelector('.world-mega').style = "display:none";
+                document.querySelector('.dot-m').style = "display:block";
+    
+                })
+                 
+
+                // slider
+
+                let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
